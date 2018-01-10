2f887aa78fbe620212ca9202045454cb1059ee36
Suyash Soni <suyash.soni248@gmail.com>
2018-01-11 01:11:40 +0530

    Transformation and action on combined RDD

--- service/data/data_feeder.py
+++ service/data/data_feeder.py
@@ -22,14 +22,18 @@ def load_neo4j_rdd(sc, neo4j_count_query, neo4j_main_query, offset=0, limit=1500
         # Node().iteritems()
         print "Loaded {} nodes to RDD @ {}".format(offset+limit, time.asctime())
         neo4j_rdd = neo4j_rdd.union(sc.parallelize(nodes))
+        break
     print "Finished at ", time.asctime()
     print "Processed {} records and loaded in RDD. Started @ {} Finished @ {}"\
         .format(total_count, start_time, time.asctime())
     return neo4j_rdd
+
+def lamb(p):
+    print p
 
 if __name__ == '__main__':
     neo4j_count_query = "MATCH (prod:Product) return COUNT(prod)"
     neo4j_main_query = "MATCH (prod:Product) return prod SKIP {offset} LIMIT {limit}"
     sc = get_or_create_spark_context("RecommenderSystem")
     neo4j_rdd = load_neo4j_rdd(sc, neo4j_count_query, neo4j_main_query)
-    #print neo4j_rdd.count()
+    #print neo4j_rdd.map(lamb).count()
